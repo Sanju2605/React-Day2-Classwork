71e102bf27ec11713a1a175b7899beaf7ba3572c
Sanju2605 <108916675+Sanju2605@users.noreply.github.com>
2023-03-29 14:55:33 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,20 @@
+import {Email, Facebook, Instagram, Twitter } from '@mui/icons-material'
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <div>
+            <Facebook/>
+            <Instagram/>
+            <Twitter/>
+            <Email/>
+            <p>
+                Contact: 7826053232
+            </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
